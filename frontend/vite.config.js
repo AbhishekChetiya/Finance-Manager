b0af89574fcf75eb443c434d6053e517fc40d85e
Abhishek Pandey <96662569+AbhishekChetiya@users.noreply.github.com>
2024-12-20 20:16:03 +0530

    resolve error of .jsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-});
+  plugins: [react()],
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+})
